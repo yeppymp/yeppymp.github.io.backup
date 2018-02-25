bb649401363dda2d9bbd155907e22e4cbe278858
Yeppy Mangun Puspitajudin <yeppymp@gmail.com>
2018-02-25 18:41:36 +0700

    First Commit

---
+++ static/js/layout.js
@@ -0,0 +1,119 @@
+var Layout = function () {
+    
+    // detect mobile device
+    var isMobileDevice = function() {
+        return  ((
+            navigator.userAgent.match(/Android/i) ||
+            navigator.userAgent.match(/BlackBerry/i) ||
+            navigator.userAgent.match(/iPhone|iPad|iPod/i) ||
+            navigator.userAgent.match(/Opera Mini/i) ||
+            navigator.userAgent.match(/IEMobile/i)
+        ) ? true : false);
+    }
+
+    // handle carousel
+    var handleCarousel = function() {
+        var $item = $('.carousel .item'); 
+        var $wHeight = $(window).height();
+        $item.eq(0).addClass('active');
+        $item.height($wHeight); 
+        $item.addClass('full-screen');
+
+        $('.carousel img').each(function() {
+            var $src = $(this).attr('src');
+            var $color = $(this).attr('data-color');
+            $(this).parent().css({
+                'background-image' : 'url(' + $src + ')',
+                'background-color' : $color
+            });
+            $(this).remove();
+        });
+
+        $(window).on('resize', function (){
+            $wHeight = $(window).height();
+            $item.height($wHeight);
+        });
+    }
+
+    // handle group element heights
+    var handleHeight = function() {
+       $('[data-auto-height]').each(function() {
+            var parent = $(this);
+            var items = $('[data-height]', parent);
+            var height = 0;
+            var mode = parent.attr('data-mode');
+            var offset = parseInt(parent.attr('data-offset') ? parent.attr('data-offset') : 0);
+
+            items.each(function() {
+                if ($(this).attr('data-height') == "height") {
+                    $(this).css('height', '');
+                } else {
+                    $(this).css('min-height', '');
+                }
+
+                var height_ = (mode == 'base-height' ? $(this).outerHeight() : $(this).outerHeight(true));
+                if (height_ > height) {
+                    height = height_;
+                }
+            });
+
+            height = height + offset;
+
+            items.each(function() {
+                if ($(this).attr('data-height') == "height") {
+                    $(this).css('height', height);
+                } else {
+                    $(this).css('min-height', height);
+                }
+            });
+
+            if(parent.attr('data-related')) {
+                $(parent.attr('data-related')).css('height', parent.height());
+            }
+       });
+    }
+
+    // Handle Work Popup
+    var handleWorkPopup = function() {
+        var overlay = $('.work-popup-overlay'),
+            close = $('.work-popup-close'),
+            trigger = $('.work-popup-trigger');
+
+        trigger.on('click', function() {
+            $(this).find('.work-popup-overlay').removeClass('work-popup-overlay-show');
+            $(this).find('.work-popup-overlay').addClass('work-popup-overlay-show');
+        });
+
+        close.on('click', function(e) {
+            e.stopPropagation();
+            overlay.removeClass('work-popup-overlay-show');
+        });
+    }
+
+    return {
+        init: function () {
+            handleCarousel(); // initial setup for carousel
+            handleHeight(); // initial setup for group element height
+            handleWorkPopup(); // initial setup for group work popup
+        },
+
+        // To get the correct viewport width based on  http://andylangton.co.uk/articles/javascript/get-viewport-size-javascript/
+        getViewPort: function() {
+            var e = window,
+                a = 'inner';
+            if (!('innerWidth' in window)) {
+                a = 'client';
+                e = document.documentElement || document.body;
+            }
+
+            return {
+                width: e[a + 'Width'],
+                height: e[a + 'Height']
+            };
+        },
+    };
+}();
+
+$(document).ready(function() {
+    Layout.init();
+});
